cf673e404873903e5c07b361f07d7c1d90433e32
vano <simplmd@gmail.com>
2023-03-28 23:15:18 +0700

    admin

--- tgbot/models/crud/product.py
+++ tgbot/models/crud/product.py
@@ -4,9 +4,32 @@ from sqlalchemy import select
 from tgbot.models.products import Product
 
 
+# async def get(
+#         session: AsyncSession,
+#         offset: Optional[int] = None,
+#         limit: int = 10
+# ):
+#     pass
+
+
+async def get_or_create(
+        session: AsyncSession,
+        name='123123'
+) -> Product:
+    to_db = select(Product).where(Product.name == name)
+    product = await session.scalar(to_db)
+    if product is None:
+        product = Product(
+            name=name,
+        )
+        session.add(product), await session.commit()
+    return product
+
+
 async def get(
         session: AsyncSession,
-        offset: Optional[int] = None,
-        limit: int = 10
-):
-    pass
+        name: str,
+) -> bool:
+    to_db = select(Product).where(Product.name == name)
+    if await session.scalar(to_db):
+        return True
